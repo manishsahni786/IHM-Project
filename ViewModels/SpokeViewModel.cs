e07578acd067b2c606beafb8790f75e5bb6562d7
Manish-Sahni <manishsahni982@gmail.com>
2023-10-15 10:21:45 +0530

    added testing function

--- ViewModels/SpokeViewModel.cs
+++ ViewModels/SpokeViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using MireWpf.Models;
 using Prism.Commands;
+using System;
 //using System;
 //using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 //using System.Linq;
 //using System.Text;
 //using System.Threading.Tasks;
@@ -698,6 +700,13 @@ namespace MireWpf.ViewModels
                         Comparator = 0,
                         Peson = 0
                     });
+        }
+
+
+
+        private void testing()
+        {
+            Debug.WriteLine("testing");
         }
     }
 }
